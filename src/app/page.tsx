78809d9ad9d51f8d934c29b43fedf30edc901a69
Ygor Oliveira <ygorluizoliveira_2012@hotmail.com>
2026-07-23 23:45:20 -0300

    Ajuste no banner altura e largura

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,16 +17,32 @@ export default function Home() {
     <main className="flex flex-1 flex-col pb-24 max-[768px]:pb-14 sm:pb-28">
       <SiteHeader />
       <section className="w-full border-y border-black/10 bg-white/75">
-        <Image
-          src="/products/banner_logo_2.png"
-          alt="Banner institucional NR Cozinhas"
-          width={1920}
-          height={560}
-          className="h-auto w-full"
-          sizes="100vw"
-          unoptimized
-          priority
-        />
+        <div className="lg:hidden">
+          <Image
+            src="/products/banner_logo_2.png"
+            alt="Banner institucional NR Cozinhas"
+            width={1920}
+            height={560}
+            className="h-auto w-full"
+            sizes="100vw"
+            unoptimized
+            priority
+          />
+        </div>
+
+        <div className="hidden lg:block">
+          <div className="relative mx-auto h-[430px] w-[860px]">
+            <Image
+              src="/products/banner_logo_2.png"
+              alt="Banner institucional NR Cozinhas"
+              fill
+              className="object-contain"
+              sizes="860px"
+              quality={100}
+              priority
+            />
+          </div>
+        </div>
       </section>
       <HeroSection />
       <EquipmentsShowcaseSection />
